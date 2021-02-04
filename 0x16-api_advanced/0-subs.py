4998bc8a1a78e0d84af5a0b43866125deff5cd30
rogerrendons <1584@holbertonschool.com>
2021-02-04 12:12:32 -0500

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Reddit API returns subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Reddit API returns subscribers"""
+    response = requests.get("https://www.reddit.com/r/{ }/about.json"
+                            .format(subreddit), headers={'User-agent': 'Hi'})
+    if not response:
+        return 0
+
+    return(response.json().get('data').get('subscribers'))
